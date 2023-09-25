4ed451a865a46a24f7c133997ffd3879b52bd833
Fabianjohn@116 <fabianjohn234@gmail.com>
2023-09-25 15:04:52 +0100

    first commit

---
+++ src/main/java/com/jonaagency/JonaRecruitmentAgencyApplication.java
@@ -0,0 +1,13 @@
+package com.jonaagency;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JonaRecruitmentAgencyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JonaRecruitmentAgencyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jonaagency/JonaRecruitmentAgencyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jonaagency;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JonaRecruitmentAgencyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
